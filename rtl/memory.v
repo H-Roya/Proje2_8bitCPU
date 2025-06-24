7fb4c4f594c3b1f3c83abc13c67d7293ab629f51
H-Roya <royahabibzade0@gmail.com>
2025-06-24 09:01:19 +0300

    modified:   modelsim/cpu_waveform.vcd 	modified:   modelsim/finalproject2.cr.mti 	modified:   modelsim/finalproject2.mpf 	modified:   modelsim/transcript 	modified:   modelsim/vsim.wlf 	modified:   modelsim/work/_info 	modified:   modelsim/work/_lib.qdb 	deleted:    modelsim/work/_lib1_16.qtl 	renamed:    modelsim/work/_lib1_16.qdb -> modelsim/work/_lib1_19.qdb 	renamed:    modelsim/work/_lib1_16.qpg -> modelsim/work/_lib1_19.qpg 	new file:   modelsim/work/_lib1_19.qtl 	modified:   rtl/memory.v 	modified:   modelsim/cpu_waveform.vcd 	modified:   modelsim/vsim.wlf

--- rtl/memory.v
+++ rtl/memory.v
@@ -7,6 +7,7 @@ module memory (
 
     reg [7:0] mem [0:255];
 
+    //add testing
     /*initial begin
         mem[0] = 8'b00010001; // LDI R0, 1
         mem[1] = 8'b00010101; // LDI R1, 5
@@ -14,18 +15,63 @@ module memory (
         mem[3] = 8'b11110000; // HALT
     end*/
 
-    initial begin
+    //branch testing doesn't branch
+    /*initial begin
         mem[0] = 8'b00010010; // LDI R0, 2
         mem[1] = 8'b00010100; // LDI R1, 4
         mem[2] = 8'b00100001; // ADD R0, R1 -> R0 = 6
         mem[3] = 8'b00110001; // SUB R0, R1 -> R0 = 2
-        mem[4] = 8'b01000001; // AND R0, R1 -> R0 = 0
-        mem[5] = 8'b01010001; // OR R0, R1 -> R0 = 4
-        mem[6] = 8'b01010111; // BRZ to 7 (should not branch because R0 ≠ 0)
-        mem[7] = 8'b00011011; // LDI R2, 3
-        mem[8] = 8'b01000100; // JMP to 4 (loop or test jump)
-        mem[9] = 8'b11110000; // HALT (unreachable in this case)
+        mem[4] = 8'b01010010; // BRZ 2 (should not branch)
+        mem[5] = 8'b00011011; // LDI R2, 3
+        mem[6] = 8'b00011100; // LDI R3, 4
+        mem[7] = 8'b01010100; // BRZ 4 (should not branch)
+        mem[8] = 8'b11110000; // HALT
+    end*/
+
+    //jump testing
+    /*initial begin
+        mem[0] = 8'b01000010; // JMP 2
+        mem[1] = 8'b00010001; // LDI R0, 1 (should be skipped)
+        mem[2] = 8'b00010011; // LDI R0, 3
+        mem[3] = 8'b11110000; // HALT
+    end*/
+
+    //brz test branches
+    /*initial begin
+        mem[0] = 8'b00010000; // LDI R0, 0
+        mem[1] = 8'b01010010; // BRZ 2 (should branch to PC=2)
+        mem[2] = 8'b00010011; // LDI R0, 3
+        mem[3] = 8'b11110000; // HALT
+
+    end*/
+
+    //final test1:
+    /*initial begin
+        mem[0] = 8'b00010010; // LDI R0, 2
+        mem[1] = 8'b01010011; // BRZ 3 (should not branch, R0 ≠ 0)
+        mem[2] = 8'b00010100; // LDI R1, 4 (should execute)
+        mem[3] = 8'b11110000; // HALT
+    end*/
+
+    //final test2:
+    /*initial begin
+        mem[0] = 8'b00010011; // LDI R0, 3
+        mem[1] = 8'b00110000; // SUB R0, R0 -> R0 = 0
+        mem[2] = 8'b01010011; // BRZ 3 (if zero, branch to HALT)
+        mem[3] = 8'b11110000; // HALT
+        mem[4] = 8'b01000001; // JMP 1 (loop back to SUB)
+    end*/
+
+    //final test3: AND OR doesn't work. Add the opcodes.
+    initial begin
+        mem[0] = 8'b00010010; // LDI R0, 2  (0010)
+        mem[1] = 8'b00010101; // LDI R1, 5  (0101)
+        mem[2] = 8'b01000001; // AND R0, R1 -> 0000
+        mem[3] = 8'b01010001; // OR R0, R1 -> 0111
+        mem[4] = 8'b11110000; // HALT
     end
+
+
 
     always @(*) begin
         data_out = mem[addr];
